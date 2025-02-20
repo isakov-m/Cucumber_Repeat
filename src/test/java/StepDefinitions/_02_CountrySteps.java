dac0c31244afb7010d2ae8b63e0db1400b61925c
isakov-m <chyraktar@gmail.com>
2025-02-21 00:08:04 +0600

    Country feature

---
+++ src/test/java/StepDefinitions/_02_CountrySteps.java
@@ -0,0 +1,36 @@
+package StepDefinitions;
+
+import Pages.DialogContent;
+import Pages.LeftNav;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.apache.commons.lang3.RandomStringUtils;
+
+public class _02_CountrySteps {
+
+    LeftNav ln = new LeftNav();
+    DialogContent dc = new DialogContent();
+
+    @And("Navigate to country")
+    public void navigateToCountry() {
+        ln.myClick(ln.setup);
+        ln.myClick(ln.parameters);
+        ln.myClick(ln.countries);
+    }
+
+    @When("Create country")
+    public void createCountry() {
+        String ulkeAdi = RandomStringUtils.randomAlphanumeric(8); // 8 karakter
+        String ulkeKodu = RandomStringUtils.randomAlphanumeric(4); // 4 karakter
+        dc.myClick(dc.addButton);
+        dc.mySendKeys(dc.nameInput,ulkeAdi);
+        dc.mySendKeys(dc.codeInput,ulkeKodu);
+        dc.myClick(dc.saveButton);
+    }
+
+    @Then("Success message should be displayed")
+    public void successMessageShouldBeDisplayed() {
+        dc.verifyContainsText(dc.successMessage,"success");
+    }
+}
