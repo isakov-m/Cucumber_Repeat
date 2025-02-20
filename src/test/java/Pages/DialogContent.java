66e634899c6b383f7636a620b1590467189539b0
isakov-m <chyraktar@gmail.com>
2025-02-21 00:08:22 +0600

    DialogContent

---
+++ src/test/java/Pages/DialogContent.java
@@ -0,0 +1,54 @@
+package Pages;
+
+import Utilities.GWD;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import java.time.Duration;
+
+public class DialogContent extends Parent {
+
+    public DialogContent() {
+        PageFactory.initElements(GWD.getDriver(), this);
+        // Sevgili PageFactory sen bu sayfanın
+        // sahibisin, senden ricam aşağıdaki locatorları
+        // ben kullanacağım zaman driver üzerinden
+        // bulup başlatıp, bana hazır et.
+        // ben artık find element le uğraşmak
+        // istemiyorum.
+    }
+
+    @FindBy(css = "input[formcontrolname='username']")
+    public WebElement username;
+
+    @FindBy(css = "input[formcontrolname='password']")
+    public WebElement password;
+
+    @FindBy(css = "button[aria-label='LOGIN']")
+    public WebElement loginButton;
+
+    @FindBy(xpath = "//div//button//div//div/span")
+    public WebElement txtStudent;
+
+    @FindBy(xpath = "//ms-add-button[contains(@tooltip,'ADD')]//button")
+    public WebElement addButton;
+
+    @FindBy(xpath = "//ms-text-field[@formcontrolname='name']//input")
+    public WebElement nameInput;
+
+    @FindBy(xpath = "//ms-text-field[@formcontrolname='code' ]//input")
+    public WebElement codeInput;
+
+    @FindBy(xpath = "//ms-save-button/button")
+    public WebElement saveButton;
+
+    @FindBy(xpath = "//div[contains(text(),'successfully')]")
+    public WebElement successMessage;
+
+
+}
