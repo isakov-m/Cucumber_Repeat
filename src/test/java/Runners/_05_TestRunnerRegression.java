54a7fbc1fcf8742a8c0ffc04c62ff3b1a916d0ae
isakov-m <chyraktar@gmail.com>
2025-02-25 02:16:28 +0600

    _05_TestRunnerRegression

---
+++ src/test/java/Runners/_05_TestRunnerRegression.java
@@ -0,0 +1,13 @@
+package Runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        // Asagidaki butun testlerden @SmokeTest tag i olanlari calistir
+        tags = "@SmokeTest or @Regression",
+        features = {"src/test/java/FeatureFiles"},
+        glue = {"StepDefinitions"}
+)
+public class _05_TestRunnerRegression extends AbstractTestNGCucumberTests {
+}
