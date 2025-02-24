58b0e7b817ab5348c4de6ce11f7f7cc31ab946fd
isakov-m <chyraktar@gmail.com>
2025-02-25 03:44:43 +0600

    Extend Rapor islemleri 2

--- src/test/java/StepDefinitions/Hooks.java
+++ src/test/java/StepDefinitions/Hooks.java
@@ -22,11 +22,12 @@ public class Hooks {
 //                senaryo);
 
         // extent reportun plugini aktif iken açık kalsın
-        if (senaryo.isFailed()){
-            TakesScreenshot ts=((TakesScreenshot) GWD.getDriver());
-            byte[] hafizadakiHali=ts.getScreenshotAs(OutputType.BYTES);
-            senaryo.attach(hafizadakiHali, "image/png", "screenshot name");
-        }
+//        if (senaryo.isFailed()){
+//            TakesScreenshot ts=((TakesScreenshot) GWD.getDriver());
+//            byte[] hafizadakiHali=ts.getScreenshotAs(OutputType.BYTES);
+//            senaryo.attach(hafizadakiHali, "image/png", "screenshot name");
+//        }
+
         // System.out.println("Senaryo bitti");
         // dogru calisirsa burada quiti cagiracagim
         GWD.quitDriver();
