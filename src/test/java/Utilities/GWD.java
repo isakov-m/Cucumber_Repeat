5f13a9413a7b82588e1cce83ca32848d19307386
isakov-m <chyraktar@gmail.com>
2025-03-14 13:10:31 +0600

    GWD upd

--- src/test/java/Utilities/GWD.java
+++ src/test/java/Utilities/GWD.java
@@ -24,7 +24,7 @@ public class GWD {
         System.setProperty("user.language", "EN");
 
         if (threadBrowserName.get()==null) // xml den çalıştırlmayan diğer bölümler
-            threadBrowserName.set("chrome"); // için default chrome olsun
+            threadBrowserName.set("firefox"); // için default chrome olsun
 
         if (threadDriver.get()==null) { // ilk kez 1 defa çalışssın
 
